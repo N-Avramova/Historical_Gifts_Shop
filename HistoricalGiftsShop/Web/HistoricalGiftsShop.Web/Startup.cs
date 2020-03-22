3d8f73c2ff7ab329bb56fa0b146e0b134369a9d5
Nevena Avramova <newenka@abv.bg>
2020-03-22 02:00:24 +0200

    SendGrid registration.

--- HistoricalGiftsShop/Web/HistoricalGiftsShop.Web/Startup.cs
+++ HistoricalGiftsShop/Web/HistoricalGiftsShop.Web/Startup.cs
@@ -57,7 +57,7 @@
             services.AddScoped<IDbQueryRunner, DbQueryRunner>();
 
             // Application services
-            services.AddTransient<IEmailSender, NullMessageSender>();
+            services.AddTransient<IEmailSender>(x => new SendGridEmailSender(this.configuration.GetValue<string>("SendGrid:ApiKey")));
             services.AddTransient<ISettingsService, SettingsService>();
         }
 
